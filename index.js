6cd380c69b44d2d110b2beed41c3909e5cae1e11
kristianvv <kristianvaag@gmail.com>
2022-09-26 16:52:11 +0200

    Flag update

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function index() {
-    prompt("Test")
-    alert("Bra test")
+function nk() {
+    if ('hidden') {
+        document.getElementById("txtbox").toggleAttribute('hidden');
+    } else {
+        document.getElementById("txtbox").toggleAttribute("hidden");
+    }
+}
+
+function ge() {
+    if ('hidden') {
+        document.getElementById("txtbox2").toggleAttribute('hidden');
+    } else {
+        document.getElementById("txtbox2").toggleAttribute("hidden");
+    }
 }
